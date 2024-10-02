799e60a871f6c0b07ea88af1c19f1b6070d361b8
Pedro <psuar92@gmail.com>
2024-10-01 21:16:53 -0500

    added lastPath functionality after logging in

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -10,9 +10,11 @@ export const LoginPage = () => {
 
     const onLogin = () => {
 
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
         login('Pedro Suarez');
 
-        navigate('/*', {
+        navigate(lastPath, {
             replace: true
         });
     }
